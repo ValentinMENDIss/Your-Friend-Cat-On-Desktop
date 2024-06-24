3c3988f53614c49e9f1d9644ac7f668ea67825a5
ValentinMENDIss <113135642+ValentinMENDIss@users.noreply.github.com>
2024-06-24 21:46:13 +0200

    Added new action for cat to desktopcat.py
    In this Commit I've added a new action for the cat. Which is running to your cursor. Have Fun :)

--- desktopcat.py
+++ desktopcat.py
@@ -1,6 +1,6 @@
 #### YOUR-FRIEND-CAT-ON-DESKTOP 
 #### MADE BY VALENTIN AND PUBLISHED ON GITHUB PAGE OF MY ACCOUNT ( ValentinMENDIss )
-#### LAST TIME EDITED ON:  23.06.2024
+#### LAST TIME EDITED ON:  24.06.2024
 ### I Hope you would enjoy. And DO NOT hurt a cat, pls. ... :)
 ## Have Fun :) 
 #
@@ -32,14 +32,14 @@ y_pos = 0
 
 # Generate random numbers
 random_pos = random.randint(0, 500)
-time_wait = random.randint(4500, 30000 )            # Time to wait ( clock variable ) is being generated in range of numbers ( 4.5 seconds and 30 seconds )     ( in ms )
+time_wait = random.randint(450, 3000 )            # Time to wait ( clock variable ) is being generated in range of numbers ( 4.5 seconds and 30 seconds )     ( in ms )
 
 
 # GET COORDINATES:
 # Display Resolution ( Gets Resolution of your Monitor / Display  Automaticly )
 DISPLAY_X = GetSystemMetrics(0)
 DISPLAY_Y = GetSystemMetrics(1)
-
+#MOUSE_POS = pyautogui.position()
 
 # Loop Variable
 run = True
@@ -69,38 +69,66 @@ clock = pygame.time.Clock()
 ## VARIABLES ##
 X_COORDINATES = 0
 Y_COORDINATES = 0
+action = random.randint(1, 100)     # Random Number ( see this as percentage % ), needed for giving an action to the cat :)
 
 
 ## MAIN LOOP ##
 while run:
     
+
+
+
+    MOUSE_POS = pyautogui.position()
+
     for event in pygame.event.get():                                                # For every Event in Pygame: 
         if event.type == pygame.QUIT:                                               # If event QUIT was used(if button quit was pressed):
             run = False
 
     window = pyautogui.getWindowsWithTitle("Cat")[0]
-    rect = window._rect  
-          
+    rect = window._rect
+    
 
-    if not abs(rect.x - X_COORDINATES) < velocity or not abs(X_COORDINATES - rect.x) < velocity:        # If rect.x coordinates - X_Coordinates is not smaller than velocity it self ( it means if it is bigger than velocity ):
-        if rect.x >= X_COORDINATES: 
-            rect.x -= velocity 
-        elif rect.x <= X_COORDINATES:
-            rect.x += velocity
+    if action in range(1, 70):       
+        if not abs(rect.x - X_COORDINATES) < velocity or not abs(X_COORDINATES - rect.x) < velocity:        # If rect.x coordinates - X_Coordinates is not smaller than velocity it self ( it means if it is bigger than velocity ):
+            if rect.x >= X_COORDINATES: 
+                rect.x -= velocity 
+            elif rect.x <= X_COORDINATES:
+                rect.x += velocity
 
-    if not abs(rect.y - Y_COORDINATES) < velocity or not abs(Y_COORDINATES - rect.y) < velocity:
-        if rect.y >= Y_COORDINATES:
-            rect.y -= velocity
-        elif rect.y <= Y_COORDINATES:
-            rect.y += velocity
+        if not abs(rect.y - Y_COORDINATES) < velocity or not abs(Y_COORDINATES - rect.y) < velocity:
+            if rect.y >= Y_COORDINATES:
+                rect.y -= velocity
+            elif rect.y <= Y_COORDINATES:
+                rect.y += velocity
 
-    if abs(rect.x - X_COORDINATES ) < velocity or abs(X_COORDINATES - rect.x ) < velocity and abs(rect.y - Y_COORDINATES) < velocity or abs(Y_COORDINATES - rect.y) < velocity:
-        pygame.time.wait(time_wait)
-        X_COORDINATES = random.randint(0, DISPLAY_X)
-        Y_COORDINATES = random.randint(0, DISPLAY_Y)
+        if abs(rect.x - X_COORDINATES ) < velocity or abs(X_COORDINATES - rect.x ) < velocity and abs(rect.y - Y_COORDINATES) < velocity or abs(Y_COORDINATES - rect.y) < velocity:
+            pygame.time.wait(time_wait)
+            X_COORDINATES = random.randint(0, DISPLAY_X)
+            Y_COORDINATES = random.randint(0, DISPLAY_Y)
+            action = random.randint(1, 100)
 
     
 
+    # accessing tuples ( x, y coordinates of the cursor)
+    MOUSE_X = MOUSE_POS[0]
+    MOUSE_Y = MOUSE_POS[1]
+
+    if action in range(71, 100):
+
+        if rect.x != MOUSE_X:
+            if rect.x >= MOUSE_X:
+                rect.x -= velocity
+            elif rect.x <= MOUSE_X:
+                rect.x += velocity
+        if rect.y != MOUSE_Y:
+            if rect.y >= MOUSE_Y:
+                rect.y -= velocity
+            elif rect.y <= MOUSE_Y:
+                rect.y += velocity
+        if rect.x == MOUSE_X and rect.y == MOUSE_Y:
+            action = random.randint(1, 100)
+
+        print(MOUSE_X, MOUSE_Y)
 
 
 
@@ -108,7 +136,7 @@ while run:
     # win32gui.MoveWindow(hwnd, 0, 0, 128, 128, True)
 
     SCREEN.fill(transparency_color)
-       
+
     SCREEN.blit(CAT, (0, 0))
 
 
